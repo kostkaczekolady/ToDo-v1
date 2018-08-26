da996e67f88a210e18e1e013b41b8695d83a6741
Karol <karolkalkus6@gmail.com>
2018-08-26 15:27:45 +0200

    trying to repair styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-
-gulp.task('sass', function () {
-  return gulp.src('scss/main.scss')
-  .pipe(sourcemaps.init())
-    .pipe(sass({
-      errLogToConsole: true,
-    outputStyle: 'expanded'
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('css'));
+gulp.task('sass', function(){
+  return gulp.src('scss/style.scss')
+    .pipe(sass({errLogToConsole: true}))
+  .pipe(gulp.dest('css'))
 });
-
-gulp.task('watch', function () {
+gulp.task('watch', function(){
   gulp.watch('scss/*.scss', ['sass']);
 });
